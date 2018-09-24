7c57f9db11acf764b556e304383032bf693abc7a
Fedoryaka Vladimir <you.bestof.thebest@gmail.com>
2018-09-24 21:24:56 +0300

    start rest integration (#1)

--- src/priceapi/views.py
+++ src/priceapi/views.py
@@ -1,8 +1,12 @@
+import requests
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-import requests
+from rest_framework import serializers, viewsets, mixins, filters
 from ipaddress import ip_address, ip_network
+
+from .models import Product, CommonInfo
+
 
 @csrf_exempt
 def hello(request):
@@ -18,7 +22,58 @@ def hello(request):
         return HttpResponseForbidden('Permission denied.')
 
     event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
-    print(event)
-    # ping 2
 
     return HttpResponse(event)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    surface = serializers.CharField(source='get_surface_display', read_only=True)
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, obj):
+        res = {}
+        for k,v in obj.get_price_for_sale.items():
+            percent = 1 + (v / 100)
+            res[k] = "{0:.2f}".format(float(obj.price) * percent)
+        return res
+
+    class Meta:
+        model = Product
+        fields = (
+            'height',
+            'surface',
+            'price',
+        )
+
+
+# Serializers define the API representation.
+class CommonInfoSerializer(serializers.ModelSerializer):
+    product_set = ProductSerializer(
+        many=True,
+        read_only=True
+    )
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, obj):
+        return '{name} {type}-{height}'.format(
+            name=obj.get_name_display(),
+            type=obj.get_type_display(),
+            height=obj.height
+        )
+
+    class Meta:
+        model = CommonInfo
+        fields = (
+            'name',
+            'product_set',
+        )
+
+
+# ViewSets define the view behavior.
+class CommonInfoViewSet(mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    queryset = CommonInfo.objects.all()
+    serializer_class = CommonInfoSerializer
+    filter_backends = (filters.OrderingFilter,)
+    ordering = ('height',)
+
