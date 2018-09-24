0ab761f9a7396a8b0888ecba9eba4a71f6d790ed
Vladimir <fedoryakavladimir@gmail.com>
2018-09-24 21:21:47 +0300

    start rest integration

--- src/priceapi/urls.py
+++ src/priceapi/urls.py
@@ -1,7 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import url, include
+
+from rest_framework import routers
 
 from .views import *
 
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'common-info', CommonInfoViewSet)
+
 urlpatterns = [
     path(r'hello/', hello, name='hello'),
+    re_path(r'^', include(router.urls)),
 ]
+
+#
+# # Wire up our API using automatic URL routing.
+# # Additionally, we include login URLs for the browsable API.
+# urlpatterns = [
+#     url(r'^', include(router.urls)),
+#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# ]
--- src/src/settings.py
+++ src/src/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'priceapi',
 ]
 
@@ -72,6 +74,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
