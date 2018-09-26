5894015035e81af012aa2fbe966c60ac462eb34e
Fedoryaka Vladimir <you.bestof.thebest@gmail.com>
2018-09-27 01:03:56 +0300

    start adding schemaorg endpoint (#7)

--- src/priceapi/urls.py
+++ src/priceapi/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, re_path
-from django.conf.urls import url, include
+from django.urls import path
+from django.conf.urls import include
 
 from rest_framework import routers
 
@@ -8,17 +8,9 @@ from .views import *
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'common-info', CommonInfoViewSet)
+router.register(r'common-info', CommonInfoViewSet, base_name='common-info')
 
 urlpatterns = [
     path(r'hello/', hello, name='hello'),
-    re_path(r'^', include(router.urls)),
+    path('', include(router.urls)),
 ]
-
-#
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     url(r'^', include(router.urls)),
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
