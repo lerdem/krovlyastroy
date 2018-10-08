e9e46b0ef0c53ae92439809ba850a4c6b2d0d780
Vladimir <fedoryakavladimir@gmail.com>
2018-10-08 23:17:37 +0300

    start adding tests

--- src/priceapi/tests.py
+++ src/priceapi/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .factories import CommonInfoFactory, ProductFactory
+
+
+class CommonInfoViewSetTests(APITestCase):
+    def test_create_account(self):
+        url = reverse('priceapi:common-info-list')
+        group_info = CommonInfoFactory()
+        ProductFactory(group=group_info)
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
