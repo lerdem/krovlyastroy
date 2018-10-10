22a6fff7c9052dd60b735b92c5c58023c805725c
Vladimir <fedoryakavladimir@gmail.com>
2018-10-10 22:51:29 +0300

    done

--- src/priceapi/tests.py
+++ src/priceapi/tests.py
@@ -8,24 +8,43 @@ from .models import CommonInfo, Product
 
 class CommonInfoViewSetTests(APITestCase):
 
-    def test_create_account(self):
-        url = reverse('priceapi:common-info-list')
+    def setUp(self):
         group_info = CommonInfoFactory(
             name=CommonInfo.NAME_CHOICES[0][0],
             type=CommonInfo.TYPE_CHOICES[2][0],
             height=57
         )
-        product = ProductFactory(
+        ProductFactory(
             group=group_info,
             height=0.45,
             surface=Product.SURFACE_CHOICES[0][0],
             price=135.25
         )
 
+    def test_get_list_products(self):
+        url = reverse('priceapi:common-info-list')
+
         response = self.client.get(url, format='json')
         data = response.data[0]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual('Профнастил Н-57',data['name'])
+        self.assertEqual(1, len(response.data))
+        self.assertEqual('Профнастил Н-57', data['name'])
+        self.assertIn('name', data)
+        self.assertIn('product_set', data)
         self.assertEqual('0.45', data['product_set'][0]['height'])
         self.assertEqual('Цинк', data['product_set'][0]['surface'])
         self.assertEqual({'50': 162, '100': 155, '1000': 148}, data['product_set'][0]['price'])
+
+    def test_get_list_schema_org(self):
+        url = reverse('priceapi:common-info-schema-org')
+
+        response = self.client.get(url, format='json')
+        data = response.data[0]
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(1, len(response.data))
+        self.assertIn('description', data)
+        self.assertIn('offers', data)
+        self.assertIn('name', data)
+        # TODO
+        # self.assertDictEqual()
+        # https://medium.com/grammofy/testing-your-python-api-app-with-json-schema-52677fe73351
