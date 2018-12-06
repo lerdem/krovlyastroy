8389e0243544e6919df2f31ac01bbd8ec9960a48
Vladimir <fedoryakavladimir@gmail.com>
2018-12-07 00:50:27 +0200

    fixes

--- fabfile.py
+++ fabfile.py
@@ -18,13 +18,11 @@ def test(c):
     cmd.append('cd {}'.format(get_rel_path()))
     if 'TRAVIS' not in os.environ:
         cmd.append('. ../venv/bin/activate')
-        cmd.append('coverage run --source=priceapi ./manage.py test')
-        cmd.append('coverage report -m')
-
     else:
-        cmd.append('/home/travis/virtualenv/python3.5.2/bin/coverage run --source=priceapi ./manage.py test')
-        cmd.append('/home/travis/virtualenv/python3.5.2/bin/coverage report -m')
+        cmd.append('source ~/virtualenv/python3.5.2/bin/activate')
 
+    cmd.append('coverage run --source=priceapi ./manage.py test')
+    cmd.append('coverage report -m')
     cmd.append('cd -')
     cmd = ' && '.join(cmd)
     c.run(cmd)
